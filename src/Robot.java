fc46affaf118381fafedf99cb85cff583ce4f1bf
Jonny Goff-White <jhg14@ic.ac.uk>
2017-01-20 01:15:07 +0000

    Implement Robot toString

--- src/Robot.java
+++ src/Robot.java
@@ -10,10 +10,27 @@ public class Robot {
     // The Robot's current direction
     private Orientation orientation;
 
+    // Whether or not the Robot has fallen off the edge
+    private boolean lost;
+
     public Robot(int x, int y, Orientation orientation) {
         this.x = x;
         this.y = y;
         this.orientation = orientation;
+        lost = false;
+    }
+
+    public boolean isLost() {
+        return lost;
+    }
+
+    public void setLost() {
+        lost = true;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y + " " + orientation + (lost ? "LOST" : "");
     }
 
 }
