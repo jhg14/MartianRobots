e08bc625901b950ba73803493d0b13acb3262883
Jonny Goff-White <jhg14@ic.ac.uk>
2017-01-20 00:28:16 +0000

    Implement Robot boilerplate

--- src/Robot.java
+++ src/Robot.java
@@ -2,4 +2,18 @@
  * Created by JHGWhite on 20/01/2017.
  */
 public class Robot {
+
+    // The Robot's location on the grid
+    private int x;
+    private int y;
+
+    // The Robot's current direction
+    private Orientation orientation;
+
+    public Robot(int x, int y, Orientation orientation) {
+        this.x = x;
+        this.y = y;
+        this.orientation = orientation;
+    }
+
 }
