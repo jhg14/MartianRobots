ab901fbe497a99fbe7e089687e47e3c29df9bde0
Jonny Goff-White <jhg14@ic.ac.uk>
2017-01-20 01:00:49 +0000

    Implement Grid boilerplate

--- src/Grid.java
+++ src/Grid.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by JHGWhite on 20/01/2017.
  */
 public class Grid {
 
+    Cell[][] grid;
 
+    List<Robot> robots;
+
+    public Grid(int upperRightx, int upperRighty) {
+
+        robots = new ArrayList<>();
+
+        int rows = upperRighty + 1;
+        int columns = upperRightx + 1;
+
+        grid = new Cell[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+    }
+
+    public void addRobot(Robot robot) {
+        robots.add(robot);
+    }
 
 }
