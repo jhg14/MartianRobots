6bcf3218d4ac8b90dd73ae1fc70a73c86c001e07
Jonny Goff-White <jhg14@ic.ac.uk>
2017-01-20 00:35:10 +0000

    Implement Cell

--- src/Cell.java
+++ src/Cell.java
@@ -2,4 +2,19 @@
  * Created by JHGWhite on 20/01/2017.
  */
 public class Cell {
+
+    boolean scented;
+
+    public Cell() {
+        scented = false;
+    }
+
+    public boolean isScented() {
+        return scented;
+    }
+
+    public void markScented() {
+        scented = true;
+    }
+
 }
